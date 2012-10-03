6ed85d811ab8cc25f409d777f190daaf671ea7a3
adrobnych <adrobnych@gmail.com>
2012-10-03 09:15:28 +0300

    loops

---
+++ loops/loops.rb
@@ -0,0 +1,38 @@
+# x = 1
+# loop do
+# 	puts "Hi"
+# 	x=x+1
+# 	break if x >=10 
+# end
+
+
+# x = 1
+# while x<10
+# 	puts "Hi" 
+# 	x = x + 1
+# end
+
+# for i in (1..10)
+# 	puts "Hi"
+# end
+
+# 10.times do
+# 	puts "Hi"
+# end
+
+# (1..10).each do
+# 	puts "Hi"
+# end
+
+# x = 1
+# puts "Hi #{x = x + 1}" while x<10
+
+x = 1
+puts "Hi #{x = x + 1}" until x>=10
+
+
+
+
+
+
+
